8f24406064ece66d936d1190ea14936c6e4ad683
Xareth <sareth2277@gmail.com>
2022-11-24 21:05:52 +0100

    Add Player factory and game object

--- script.js
+++ script.js
@@ -2,28 +2,77 @@
 // Tic-Tac-Toe
 // ==========================================================================
 
+"use strict"
+
+const testButton = document.querySelector('.testButton');
+
 const gameBoard = (() => {
-    const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    const board = ["", "", "", "", "", "", "", "", ""];
     
-    function setupBoard(board) {
+    function setupBoard() {
         const main = document.querySelector('.main');
 
-        for (c = 1; c <= (board.length); c++) {
+        for (let c = 0; c < (board.length); c++) {
             const cell = document.createElement('div');
             cell.setAttribute('id', c);
-            // cell.addEventListener('click', );
+            cell.addEventListener('click', setField);
             main.appendChild(cell).className = "cell";
         };
     };
+    
+    function setField(e) {
+        const targetID = e.target.getAttribute('id');   
+        board[targetID] = 'X';
+        e.target.textContent = 'X';
+        game.switchTurns();
+        // check if field is already occupied
+    };
+    
+    // function for restarting the game
 
-    setupBoard(board);
-    // return {board, setupBoard};
+    return { setupBoard };
+    
+})();
+
+const Player = (name, playerTurn) => {
+    playerTurn = false;
+    return { name, playerTurn };
+};
+
+const game = (() => {
+    
+    // Init
+    gameBoard.setupBoard();
+    let playerOne;
+    let playerTwo;
+    askName();
+    
+    function askName() {
+        const playerOneName = prompt("Player 1 name: ");
+        const playerTwoName = prompt("Player 2 name: ");
+        playerOne = Player(playerOneName);
+        playerOne.playerTurn = true;    //player one starts the game
+        playerTwo = Player(playerTwoName);
+    };
+    
+    function switchTurns() {
+        if (playerOne.playerTurn) {
+            playerOne.playerTurn = false;
+            playerTwo.playerTurn = true;
+        } else if (playerTwo.playerTurn) {
+            playerOne.playerTurn = true;
+            playerTwo.playerTurn = false;
+        };
+        console.log(playerOne.playerTurn);
+    };
+    
+    // check for win or draw with message about the outcome
+
+    return { switchTurns };
 
 })();
 
-
-// const Player = (name) => {
-//     let name = name;
-//     return {name};
-// };
+testButton.addEventListener("click", () => {
+    game.switchTurns(game.playerOne, game.playerTwo);
+});
 
